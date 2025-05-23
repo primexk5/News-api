d7b3160faee0bdaba6ffd382206a1ad13682c976
Lapshak Barnabas <lapshakbarnabas4@gmail.com>
2025-05-23 14:46:13 +0100

    first commit

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.patch('/api/update:id', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
